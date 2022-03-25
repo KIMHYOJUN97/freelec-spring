f3de0bed7ad3a31006d46123b15b714b6343f6c8
KIMHYOJUN <olzitoon@naver.com>
2022-03-25 13:12:20 +0900

    day1

---
+++ freelec-spring/src/main/java/com/example/freelecspring/FreelecSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.freelecspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecSpringApplication.class, args);
+    }
+
+}
---
+++ freelec-spring/src/test/java/com/example/freelecspring/FreelecSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelecspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
