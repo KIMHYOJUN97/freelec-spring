cea081946b0739524564197293dbbdd04f8d87b4
KIMHYOJUN <olzitoon@naver.com>
2022-03-27 13:57:20 +0900

    day3

--- freelec-spring/src/main/java/com/example/freelecspring/web/dto/IndexController.java
+++ freelec-spring/src/main/java/com/example/freelecspring/web/dto/IndexController.java
@@ -13,8 +13,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts",postsService.findAllDesc);
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
